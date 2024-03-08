a19098b44eb48aeaa2e0f1b12df837ccffc190da
rajivsen511 <rajivsen511@gmail.com>
2024-03-08 14:51:19 +0530

    Banking app springboot program

---
+++ banking-app/banking-app/src/main/java/net/javaguides/banking/service/AccountService.java
@@ -0,0 +1,9 @@
+package net.javaguides.banking.service;
+
+import net.javaguides.banking.dto.AccountDto;
+
+public interface AccountService {
+	
+	AccountDto createAccount(AccountDto accountDto);
+
+}
---
+++ banking-app/banking-app/src/main/java/net/javaguides/banking/service/impl/AccountServiceImpl.java
@@ -0,0 +1,27 @@
+package net.javaguides.banking.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import net.javaguides.banking.dto.AccountDto;
+import net.javaguides.banking.entity.Account;
+import net.javaguides.banking.mapper.AccountMapper;
+import net.javaguides.banking.repository.AccountRepository;
+import net.javaguides.banking.service.AccountService;
+@Service
+public class AccountServiceImpl implements AccountService {
+ private AccountRepository accountRepository;
+ 
+ public AccountServiceImpl(AccountRepository accountRepository) {
+	 this.accountRepository=accountRepository;
+	 
+ }
+ @Override
+ public AccountDto createAccount(AccountDto accountDto) {
+	Account account=AccountMapper.mapToAccount(accountDto);
+			Account savedAccount=accountRepository.save(account);
+			return AccountMapper.mapToAccountDto(savedAccount);
+		
+			
+ }
+ 
+}
